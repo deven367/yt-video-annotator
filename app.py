1581a28b77405ba83e856267fb1dd138226cc378
Ideapad-310 <masterdeven@gmail.com>
2023-01-23 21:38:06 -0500

    first draft of the app

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from annotator.utils import *
+st.set_page_config(layout='wide')
+from fastcore.xtras import globtastic
+from pathlib import Path
+
+SRT_PATH = Path('srt')
+if not SRT_PATH.exists(): SRT_PATH.mkdir(exist_ok=True)
+
+def make_sidebar():
+    with st.sidebar:
+        st.write('App')
+        st.write('YouTube')
+
+
+def main():
+    make_sidebar()
+    # st.write('This is it!')
+    url = st.text_input('Enter URL for the YT video')
+
+    if st.button('Generate SRT'):
+        # audio_src = get_audio(url)
+        audio_path = Path('./audio')
+        audio_src = globtastic(audio_path, file_glob='*.mp3')[0]
+        result = annotate(audio_src)
+        df = df_from_result(result)
+
+        # st.write(result.get('segments', 'wrong key'))
+        st.write(df)
+        name = Path(audio_src).stem
+        s = generate_srt(df)
+        with working_directory(SRT_PATH):
+            write_srt(s, name)
+
+        with working_directory(SRT_PATH):
+            srt = globtastic('.', file_glob='*.srt')[0]
+            with open(srt) as f:
+                st.download_button('Download SRT', f, file_name=f'{name}.srt')
+
+
+if __name__ == "__main__":
+    main()
